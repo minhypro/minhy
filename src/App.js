37dd3dcb94dec6034b03645b480d494d13a60076
Minh Y <85498660+minhypro@users.noreply.github.com>
2022-08-30 10:24:20 +0700

    add scroll progress

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import About from './components/About'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import { motion, useScroll, useSpring } from "framer-motion"
+
+function ScrollProgress() {
+  const { scrollYProgress } = useScroll();
+  // style={{ scaleX: scrollYProgress}}
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+  
+  return (
+    <motion.div className='scroll-progress' style={{ scaleX }} />  
+  )
+}
 
 
 function App() {
   
   return (
-    <div className='App pb-6 bg-slate-800 text-white'>
+    <div className='App pb-6 bg-slate-800 text-white relative'>
+      <ScrollProgress/>
       <Header />
       <Hero/>
       <About/>
