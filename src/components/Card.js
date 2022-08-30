54f086f03ab075f2b6bbc4033d39650646214b37
Minh Y <85498660+minhypro@users.noreply.github.com>
2022-08-30 08:54:45 +0700

    add project section

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function Card({ img, title, content }) {
+function Card({ link, img, title, content }) {
   return (
-    <div className='flex flex-col hover:scale-102 hover:cursor-pointer transition-all ease-linear duration-200 text-left'>
-      <img src={img} alt='' />
-      <div className='p-8'>
-        <h3 className='text-2xl mb-2 font-bold'>{title}</h3>
-        <p className='text-gray-300 text-lg'>{content}</p>
-      </div>
+    <div className='group flex flex-col hover:scale-102 hover:cursor-pointer transition-all ease-linear duration-200 text-left'>
+      <a href={link}>
+        <img src={img} alt='' />
+        <div className='p-8'>
+          <h3 className='text-2xl mb-2 font-bold origin-left group-hover:translate-x-1 group-hover:text-sky-500 group-hover:scale-105 transition-all ease-linear duration-200'>{title}</h3>
+          <p className='text-gray-300 text-lg'>{content}</p>
+        </div>
+      </a>
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,53 @@
 import React from 'react'
 import Masonry from 'react-masonry-css'
 import Card from '../components/Card'
+import ycommerce from '../assets/images/ycommerce.png'
+import ymovie from '../assets/images/ymovie.png'
+import airbnb from '../assets/images/airbnb.png'
+import gamefi from '../assets/images/gamefi.png'
+import drumkit from '../assets/images/drumkit.png'
+import theband from '../assets/images/theband.png'
 
 function Projects() {
+
+  const myprojects = [
+    {
+      title: 'Ycommerce',
+      content: 'A MERN stack eCommerce website that I handle all the things on both Frontend and Backend side with the latest technologies. Full-function shopping cart and many more features',
+      img: ycommerce,
+      link: "https://ycommerce.herokuapp.com/"
+    },
+    {
+      title: 'Ymovie',
+      content: 'A daily updated Movie info website (include watch trailer and hidden cam version) with ReactJS and API get from themoviedb and other resources',
+      img: ymovie,
+      link: "https://62e15373b6e64727675684d9--cozy-starlight-bd6a7d.netlify.app/"
+    },
+    {
+      title: 'AirBnb clone',
+      content: 'HTML, CSS and pure JavaScript to clone AirBnb website with fully responsive',
+      img: airbnb,
+      link: 'https://minhypro.github.io/clone-air-bnb/'
+    },
+    {
+      title: 'GameFi',
+      content: 'My first self-built website with no tutorial by HTML, CSS and pure JavaScript. Cool animation and fully responsive',
+      img: gamefi,
+      link: 'https://minhypro.github.io/game-fi-clone/'
+    },
+    {
+      title: 'Drum kit',
+      content: 'Play drums by typing the keyboard. I made with supporting from Angela Yu from The App Brewer',
+      img: drumkit,
+      link: 'https://minhypro.github.io/drum-kit/'
+    },
+    {
+      title: 'The Band',
+      content: 'The entry level website template in W3School. Inspired by Son Dang from F8 with love 💖',
+      img: theband,
+      link: 'https://minhypro.github.io/w3-the-band/'
+    }
+  ]
 
   return (
     <div id='projects' className='container mx-auto pt-20'>
@@ -16,26 +61,14 @@ function Projects() {
         columnClassName='my-masonry-grid_column'
       >
         <h2 className='mt-5 mb-20 text-5xl font-bold'>Work that I've done for the past 6 months</h2>
-        <Card
-          img='https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1080.jpeg'
-          title='CoinView mobile App'
-          content='I was create it by myselft'
-        />
-        <Card
-          img='https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1080.jpeg'
-          title='CoinView mobile App'
-          content='I was create it by myselft'
-        />
-        <Card
-          img='https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1080.jpeg'
-          title='CoinView mobile App'
-          content='I was create it by myselft'
-        />
-        <Card
-          img='https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1080.jpeg'
-          title='CoinView mobile App'
-          content='I was create it by myselft'
-        />
+        {myprojects.map(project => (
+          <Card
+          img={project.img}
+          title={project.title}
+          content={project.content}
+          link={project.link}
+          />
+        ))}
       </Masonry>
       
     </div>
